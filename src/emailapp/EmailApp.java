f6786e621a7db92e7cfe3e23e6a126b99f5c553f
Hasancan Midi <Hasancanmidi@hotmail.com>
2021-01-25 14:54:43 +0300

    Initial commit

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,10 @@
+package emailapp;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class EmailApp {
+    public static void main(String[] args) {
+    Email em1 = new Email("HasanCan","Midi");
+        System.out.println(em1.showInfo());
+    }
+}
